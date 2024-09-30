52edcda40d198992a1cc41aff2627a3132c9b040
mimiTsuki <49679298+mimiTsuki@users.noreply.github.com>
2024-09-30 16:35:16 +0900

    fzfのデフォルト設定を追加

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -1,2 +1,6 @@
+# zsh
 source $(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh
 source $(brew --prefix)/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+
+# fzf
+export FZF_DEFAULT_OPTS='--layout reverse'
